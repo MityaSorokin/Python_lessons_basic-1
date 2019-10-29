9c4bae8582e3e32ef647d1d6972be747d3776b45
MityaSorokin <genrymorli@gmail.com>
2019-10-29 11:26:27 +0500

    использовал вместо ветвления исключение

--- lesson03/lesson_3.1.py
+++ lesson03/lesson_3.1.py
@@ -7,10 +7,24 @@
 
 
 def div(*args):
-    arg1 = int(input("Input dividend "))
-    arg2 = int(input("Input divider "))
+
+    try:
+        arg1 = int(input("Input dividend "))
+        arg2 = int(input("Input divider "))
+        res = arg1 / arg2
+    except ValueError:
+        return 'Value error'
+    except ZeroDivisionError:
+        return "Wrong devider! You can't use zero as a devider"
+
+    return res
+
+    '''
     if arg2 != 0:
         return arg1 / arg2
     else:
         print("Wrong number! Devider can't be null")
+    '''
+
+
 print(f'result  {div()}')
